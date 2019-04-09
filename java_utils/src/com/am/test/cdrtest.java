cf0393731ca9f13c522be5599e22fb918cdba735
am <brickai@evfort.com>
2019-04-09 09:23:24 +0800

    set 1

--- java_utils/src/com/am/test/cdrtest.java
+++
@@ -1,5 +0,0 @@
-package com.am.test;
-
-public class cdrtest {
-
-}
--- java_utils/src/com/am/test/tt.java
+++
@@ -1,9 +0,0 @@
-package com.am.test;
-
-public class tt {
-
-	public static void main(String[] args) {
-		System.out.println("ffffffff");
-	}
-
-}
